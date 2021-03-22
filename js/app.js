f5cd69015626415f71bb697d5cd3a25c05d25dd2
Puagh <pablofeijo181@gmail.com>
2021-03-21 23:45:40 -0300

    medio mobile first

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const nav = document.getElementById("nav-bar");
+const hambBtn = document.getElementById("hamb-btn");
+
+hambBtn.addEventListener('click', function () {
+    nav.classList.toggle("block"); 
+    });
